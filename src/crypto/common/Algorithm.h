10497d5bb09ae5e1eb69e52294149ad767dd5e65
Tony Butler <spudz76@gmail.com>
2021-04-17 08:28:14 -0600

    Rename Defyx->Panthera

--- src/crypto/common/Algorithm.h
+++ src/crypto/common/Algorithm.h
@@ -62,12 +62,11 @@ public:
         RX_SFX          = 0x72151273,   // "rx/sfx"           RandomSFX (Safex Cash).
         RX_KEVA         = 0x7214116b,   // "rx/keva"          RandomKEVA (Keva).
         RX_YADA         = 0x72151279,   // "rx/yada"          RandomYada (YadaCoin).
+        RX_XLA          = 0x721211ff,   // "panthera"         Panthera (Scala2).
         AR2_CHUKWA      = 0x61130000,   // "argon2/chukwa"    Argon2id (Chukwa).
         AR2_CHUKWA_V2   = 0x61140000,   // "argon2/chukwav2"  Argon2id (Chukwa v2).
         AR2_WRKZ        = 0x61120000,   // "argon2/wrkz"      Argon2id (WRKZ)
         KAWPOW_RVN      = 0x6b0f0000,   // "kawpow/rvn"       KawPow (RVN)
-
-        RX_XLA          = 0x721211ff,   // "panthera"         Panthera (Scala2).
     };
 
     enum Family : uint32_t {
